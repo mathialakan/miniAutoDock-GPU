56c4c777874e6e9fb075cf16b902be1663d4be30
Mathialakan Thavappiragasam <mathialakan@gmail.com>
2020-07-06 16:25:05 -0400

    initial commit with the cuda version

---
+++ README.md
@@ -0,0 +1,64 @@
+#miniAutoDock-GPU 
+#================ 
+
+ A miniapp based on AutoDock-GPU https://github.com/ccsb-scripps/AutoDock-GPU\ 
+
+This app mainly focuses the performance portability and targets NVIDIA and AMD GPUs 
+ 
+
+#Programming Base (API/ Model) 
+
+* CUDA 
+* HIP 
+* Kokkos 
+
+ 
+
+# Compilation 
+
+```zsh 
+make DEVICE=<TYPE> NUMWI=<NWI> CARD=<VENDOR> 
+``` 
+|  Prameters | Description            | Values                                             |
+|:----------:|:----------------------:|:--------------------------------------------------:|
+| `<TYPE>`   | Accelerator chosen     | `CPU`, `GPU`                                       |
+| `<NWI>`    | OpenCL work-group size | `1`, `2`, `4`, `8`, `16`, `32`, `64`, `128`, `256` |
+| `<CARD>`   | GPU vendor/typ         | `NVIDIA`, `AMD`                                    |
+
+
+After successful compilation, the host binary **autodock_&lt;type&gt;_&lt;N&gt;wi** is placed under [bin](./bin).
+
+| Binary-name portion | Description            | Values                                            |
+|:-------------------:|:----------------------:|:-------------------------------------------------:|
+| **&lt;type&gt;**    | Accelerator chosen     | `gpu`                                      |
+| **&lt;N&gt;**       | OpenCL work-group size | `1`, `2`, `4`, `8`,`16`, `32`, `64`, `128`, `256` |
+
+
+
+## Basic command 
+
+```zsh 
+./bin/autodock_<type>_<N>wi \ 
+-ffile <protein>.maps.fld \ 
+-lfile <ligand>.pdbqt \ 
+-nrun <nruns> 
+``` 
+
+| Mandatory options | Description   | Value                     |
+|:-----------------:|:-------------:|:-------------------------:|
+| -ffile            |Protein file   |&lt;protein&gt;.maps.fld   |
+| -lfile            |Ligand file    |&lt;ligand&gt;.pdbqt       |
+| -nrun		    |               |                           |	
+
+## Example 
+
+```zsh 
+./bin/autodock_gpu_64wi \ 
+-ffile ./input/1stp/derived/1stp_protein.maps.fld \ 
+-lfile ./input/1stp/derived/1stp_ligand.pdbqt \ 
+-nrun 10 
+``` 
+
+ 
+
+ 
---
+++ host/src/main.cpp
@@ -0,0 +1,180 @@
+/*
+
+AutoDock-GPU, an OpenCL implementation of AutoDock 4.2 running a Lamarckian Genetic Algorithm
+Copyright (C) 2017 TU Darmstadt, Embedded Systems and Applications Group, Germany. All rights reserved.
+For some of the code, Copyright (C) 2019 Computational Structural Biology Center, the Scripps Research Institute.
+
+AutoDock is a Trade Mark of the Scripps Research Institute.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <vector>
+
+#include "processgrid.h"
+#include "processligand.h"
+#include "getparameters.h"
+#include "performdocking.h"
+#include "setup.hpp"
+#include "simulation_state.hpp"
+#include "GpuData.h"
+
+#ifndef _WIN32
+// Time measurement
+#include <sys/time.h>
+#endif
+
+template<typename T>
+inline double seconds_since(T& time_start)
+{
+#ifndef _WIN32
+	timeval time_end;
+	gettimeofday(&time_end,NULL);
+        double num_sec     = time_end.tv_sec  - time_start.tv_sec;
+        double num_usec    = time_end.tv_usec - time_start.tv_usec;
+        return (num_sec + (num_usec/1000000));
+#else
+	return 0.0;
+#endif
+}
+
+template<typename T>
+inline void start_timer(T& time_start)
+{
+#ifndef _WIN32
+	gettimeofday(&time_start,NULL);
+#endif
+}
+
+int main(int argc, char* argv[])
+{
+	// Timer initializations
+#ifndef _WIN32
+	timeval time_start, idle_timer;
+	start_timer(time_start);
+	start_timer(idle_timer);
+#else
+	// Dummy variables if timers off
+	double time_start, idle_timer;
+#endif
+	double total_setup_time=0;
+	double total_processing_time=0;
+	double total_exec_time=0;
+	double idle_time;
+
+	// Setup master map set (one for now, nthreads-1 for general case)
+	std::vector<Map> all_maps;
+
+	// Objects that are arguments of docking_with_gpu
+	GpuData cData;
+	GpuTempData tData;
+
+	cData.devnum=-1;
+	// Get device number to run on
+	for (unsigned int i=1; i<argc-1; i+=2)
+	{
+		if (strcmp("-devnum", argv [i]) == 0)
+		{
+			int tempint;
+			sscanf(argv [i+1], "%lu", &tempint);
+			if ((tempint >= 1) && (tempint <= 65536))
+				cData.devnum = (unsigned long) tempint-1;
+			else
+				printf("Warning: value of -devnum argument ignored. Value must be an integer between 1 and 65536.\n");
+			break;
+		}
+	}
+
+	// Error flag
+	int err = 0;
+
+	// Print version info
+	printf("AutoDock-GPU version: %s\n", VERSION);
+
+	setup_gpu_for_docking(cData,tData);
+
+		int t_id = 0;
+		Dockpars   mypars;
+		Liganddata myligand_init;
+		Gridinfo   mygrid;
+		Liganddata myxrayligand;
+		std::vector<float> floatgrids;
+	        SimulationState sim_state;
+#ifndef _WIN32
+	        timeval setup_timer, exec_timer, processing_timer;
+#else
+		double setup_timer, exec_timer, processing_timer;
+#endif
+		int i_job = 0;
+			start_timer(setup_timer);
+			// Load files, read inputs, prepare arrays for docking stage
+			if (setup(all_maps, mygrid, floatgrids, mypars, myligand_init, myxrayligand, i_job, argc, argv) != 0) {
+				// If error encountered: Set error flag to 1; Add to count of finished jobs
+				// Keep in setup stage rather than moving to launch stage so a different job will be set up
+				printf("\n\nError in setup of the Job \n");
+				err = 1;
+			} else { // Successful setup
+				// Copy preloaded maps to GPU
+				total_setup_time+=seconds_since(setup_timer);
+			}
+
+			// Starting Docking
+			int error_in_docking;
+			// Critical section to only let one thread access GPU at a time
+			{
+				printf("\nRunning the Job \n");
+				//  start exec timer
+	                        start_timer(exec_timer);
+				// Dock
+				error_in_docking = docking_with_gpu(&(mygrid), floatgrids.data(), &(mypars), &(myligand_init), &(myxrayligand), &argc, argv, sim_state, cData, tData);
+				// End exec timer, start idling timer
+				sim_state.exec_time = seconds_since(exec_timer);
+			}
+
+			if (error_in_docking!=0){
+				// If error encountered: Set error flag to 1; Add to count of finished jobs
+				// Set back to setup stage rather than moving to processing stage so a different job will be set up
+				printf("\n\nError in docking_with_gpu, stopped the Job \n");
+				err = 1;
+			} else { // Successful run
+#ifndef _WIN32
+				total_exec_time+=sim_state.exec_time;
+				printf("\nThe Job took %.3f sec for setup\n", sim_state.exec_time);
+#endif
+			}
+
+			// Post-processing
+	                start_timer(processing_timer);
+	                process_result(&(mygrid), floatgrids.data(), &(mypars), &(myligand_init), &(myxrayligand), &argc,argv, sim_state);
+	                total_processing_time+=seconds_since(processing_timer);
+
+	finish_gpu_from_docking(cData,tData);
+
+#ifndef _WIN32
+	// Total time measurement
+	printf("\nRun time of the job: %.3f sec", seconds_since(time_start));
+#endif
+
+	// Alert user to ligands that failed to complete
+	if (err==1){
+		printf("\nThe job was not successful.");
+	}
+	return 0;
+}
